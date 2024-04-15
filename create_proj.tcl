0bdaab008a95c2ab9c8707aa72475cc0f5d42da3
roajpg <roajonny@gmail.com>
2024-04-15 10:40:53 -0700

    Updates comment syntax in proj init script and updates readme

--- create_proj.tcl
+++ create_proj.tcl
@@ -1,16 +1,16 @@
-// File :            create_proj.tcl
-// Title :           create_proj
-//
-// Author(s) :       Jonathan Roa
-//
-// Description :     Generic Vivado project build script that initializes 
-//                   .xpr w/ relevant sources and any block designs + wrappers
-//
-// Revisions 
-//
-// Date        Name            REV#        Description 
-// ----------  --------------- ----------- -------------------------------------------
-// (04/14/24)  Jonathan Roa    1.0         Initial Revision
+# File :            create_proj.tcl
+# Title :           create_proj
+# 
+# Author(s) :       Jonathan Roa
+# 
+# Description :     Generic Vivado project build script that initializes 
+#                   .xpr w/ relevant sources and any block designs + wrappers
+# 
+# Revisions 
+# 
+# Date        Name            REV#        Description 
+# ----------  --------------- ----------- -------------------------------------------
+# (04/14/24)  Jonathan Roa    1.0         Initial Revision
 
-// Initialize the project
+# Initialize the project
 source proj_init.tcl
