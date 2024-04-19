c62f2dca466c1952ec6cb14103f7470ccd1fa7ef
roajpg <roajonny@gmail.com>
2024-04-19 16:20:26 -0700

    Cleans UART RX TB formatting

--- proj_srcs/hdl/uart_rx_tb.v
+++ proj_srcs/hdl/uart_rx_tb.v
@@ -44,7 +44,7 @@ module uart_rx_tb ();
         .o_tx_done    (w_tx_done)
     );
 
-   uart_rx UUT_uart_rx (
+    uart_rx UUT_uart_rx (
         .i_clk        (r_clk),
         .i_rst_n      (r_rst_n),
         .i_baud_sel   (r_baud_sel),
@@ -53,97 +53,97 @@ module uart_rx_tb ();
         .o_rx_data    (w_rx_data),
         .o_rx_running (w_rx_running),
         .o_rx_done    (w_rx_done)
-   ); 
+    ); 
 
-   // Generate the clock
-   always begin
-       #(p_CLK_PERIOD/2); r_clk <= ~r_clk;
-   end
+    // Generate the clock
+    always begin
+        #(p_CLK_PERIOD/2); r_clk <= ~r_clk;
+    end
 
-   initial begin
+    initial begin
 
-       // Test 1: 9600 baud
-       init();
-       set_baud(3'b000);
-       assert_reset();
-       transmit(8'hAA); wait_5ms();
-       transmit(8'h7B); wait_5ms();
-       transmit(8'h03); wait_5ms();
-       transmit(8'h28); wait_5ms();
-       transmit(8'h12); wait_5ms();
+        // Test 1: 9600 baud
+        init();
+        set_baud(3'b000);
+        assert_reset();
+        transmit(8'hAA); wait_5ms();
+        transmit(8'h7B); wait_5ms();
+        transmit(8'h03); wait_5ms();
+        transmit(8'h28); wait_5ms();
+        transmit(8'h12); wait_5ms();
 
-       // Test 2: 4800 baud
-       init();
-       set_baud(3'b001);
-       assert_reset();
-       transmit(8'hAA); wait_5ms();
-       transmit(8'h7B); wait_5ms();
-       transmit(8'h03); wait_5ms();
-       transmit(8'h28); wait_5ms();
-       transmit(8'h12); wait_5ms();
+        // Test 2: 4800 baud
+        init();
+        set_baud(3'b001);
+        assert_reset();
+        transmit(8'hAA); wait_5ms();
+        transmit(8'h7B); wait_5ms();
+        transmit(8'h03); wait_5ms();
+        transmit(8'h28); wait_5ms();
+        transmit(8'h12); wait_5ms();
 
-       // Test 3: 19200 baud
-       init();
-       set_baud(3'b010);
-       assert_reset();
-       transmit(8'hAA); wait_5ms();
-       transmit(8'h7B); wait_5ms();
-       transmit(8'h03); wait_5ms();
-       transmit(8'h28); wait_5ms();
-       transmit(8'h12); wait_5ms();
+        // Test 3: 19200 baud
+        init();
+        set_baud(3'b010);
+        assert_reset();
+        transmit(8'hAA); wait_5ms();
+        transmit(8'h7B); wait_5ms();
+        transmit(8'h03); wait_5ms();
+        transmit(8'h28); wait_5ms();
+        transmit(8'h12); wait_5ms();
 
-       // Test 4: 57600 baud
-       init();
-       set_baud(3'b011);
-       assert_reset();
-       transmit(8'hAA); wait_5ms();
-       transmit(8'h7B); wait_5ms();
-       transmit(8'h03); wait_5ms();
-       transmit(8'h28); wait_5ms();
-       transmit(8'h12); wait_5ms();
+        // Test 4: 57600 baud
+        init();
+        set_baud(3'b011);
+        assert_reset();
+        transmit(8'hAA); wait_5ms();
+        transmit(8'h7B); wait_5ms();
+        transmit(8'h03); wait_5ms();
+        transmit(8'h28); wait_5ms();
+        transmit(8'h12); wait_5ms();
 
-       // Test 5: 115200 baud
-       init();
-       set_baud(3'b100);
-       assert_reset();
-       transmit(8'hAA); wait_5ms();
-       transmit(8'h7B); wait_5ms();
-       transmit(8'h03); wait_5ms();
-       transmit(8'h28); wait_5ms();
-       transmit(8'h12); wait_5ms();
-   end
+        // Test 5: 115200 baud
+        init();
+        set_baud(3'b100);
+        assert_reset();
+        transmit(8'hAA); wait_5ms();
+        transmit(8'h7B); wait_5ms();
+        transmit(8'h03); wait_5ms();
+        transmit(8'h28); wait_5ms();
+        transmit(8'h12); wait_5ms();
+    end
 
-   task transmit(input [7:0] tx_data); begin
-       r_tx_data <= tx_data;
-       #(p_CLK_PERIOD);
-       r_wr_en <= 1'b1; #(p_CLK_PERIOD);
-       r_wr_en <= 1'b0;
-   end
-   endtask
+    task transmit(input [7:0] tx_data); begin
+        r_tx_data <= tx_data;
+        #(p_CLK_PERIOD);
+        r_wr_en <= 1'b1; #(p_CLK_PERIOD);
+        r_wr_en <= 1'b0;
+    end
+    endtask
 
-   task assert_reset(); begin
-       #(p_CLK_PERIOD*5); r_rst_n <= 1'b0;
-       #(p_CLK_PERIOD*5); r_rst_n <= 1'b1;
-   end
-   endtask;
+    task assert_reset(); begin
+        #(p_CLK_PERIOD*5); r_rst_n <= 1'b0;
+        #(p_CLK_PERIOD*5); r_rst_n <= 1'b1;
+    end
+    endtask;
 
-   task wait_5ms(); begin
-       #(p_CLK_PERIOD*625000);
-   end
-   endtask
+    task wait_5ms(); begin
+        #(p_CLK_PERIOD*625000);
+    end
+    endtask
 
-   task set_baud(input [p_BAUD_SEL_WIDTH-1:0] baud_sel); begin
-       r_baud_sel <= baud_sel;
-   end
-   endtask
+    task set_baud(input [p_BAUD_SEL_WIDTH-1:0] baud_sel); begin
+        r_baud_sel <= baud_sel;
+    end
+    endtask
 
-   task init(); begin
-       r_clk      <= 1'b1;
-       r_rst_n    <= 1'b1;
-       r_baud_sel <= {p_BAUD_SEL_WIDTH{1'b0}};
-       r_tx_data  <= 1'b1;
-       r_wr_en    <= 1'b0;
-   end
-   endtask
+    task init(); begin
+        r_clk      <= 1'b1;
+        r_rst_n    <= 1'b1;
+        r_baud_sel <= {p_BAUD_SEL_WIDTH{1'b0}};
+        r_tx_data  <= 1'b1;
+        r_wr_en    <= 1'b0;
+    end
+    endtask
    
 endmodule
